d94f2a6fcac2e10bcf91ecbab1ed1ace23febabf
lyashov <awardsw081111>
2020-01-10 15:56:28 +0300

    +

---
+++ src/main/java/com/lyaev/kafka/SpringKafkaH2/SpringKafkaH2Application.java
@@ -0,0 +1,13 @@
+package com.lyaev.kafka.SpringKafkaH2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/lyaev/kafka/SpringKafkaH2/SpringKafkaH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyaev.kafka.SpringKafkaH2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
